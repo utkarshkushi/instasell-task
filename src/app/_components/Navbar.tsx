8ada1588db65e28fe3cbe22b4f5907cec59465ab
utkarshkuhsi <utkarshsuneela@gmail.com>
2024-01-22 19:51:49 +0530

    addded dropdown menu to the tracking pages link of navbar

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -30,14 +30,16 @@ const Navbar = (props: Props) => {
                 <a className='group text-[#4A4A4A] transition-all cursor-pointer duration-300 ease-in-out' href='#'>
                     <span onClick={() => setPagesOpen((prev) => !prev)} className='bg-left-bottom pb-2 font-inter flex items-center gap-2 font-semibold bg-gradient-to-r from-[#8051FF] to-[#8051FF] bg-[length:0%_3px] bg-no-repeat group-hover:bg-[length:50%_3px] transition-all duration-500 ease-out'>
                         Tracking Page <ChevronDown size={20} color='#4A4A4A' />
-                        <div className='w-56 px-5 py-5'>
+                        {pagesOpen && (
+                            <div className='w-40 px-5 py-2 absolute top-[60px] left-[260px] bg-white z-[100]'>
                             <div className='flex flex-col gap-5'>
-                                <p className='text-md font-roboto cursor-pointer hover:text-[#4f2e90] hover:border-l-4 hover:border-[#4f2e90] pl-2 flex gap-2'> Page-1</p>
-                                <p className='text-md font-roboto cursor-pointer hover:text-[#4f2e90] hover:border-l-4 hover:border-[#4f2e90] pl-2 flex gap-2'> Page-2</p>
-                                <p className='text-md font-roboto cursor-pointer hover:text-[#4f2e90] hover:border-l-4 hover:border-[#4f2e90] pl-2 flex gap-2'> Refer Your Friend</p>
-                                <p className='text-md font-roboto cursor-pointer hover:text-[#4f2e90] hover:border-l-4 hover:border-[#4f2e90] pl-2 flex gap-2'> Apply coupon code</p>
+                                <p className='text-md font-roboto cursor-pointer hover:text-[#8051FF] hover:border-l-4 hover:border-[#8051FF] pl-2 flex gap-2'> Page-1</p>
+                                <p className='text-md font-roboto cursor-pointer hover:text-[#8051FF] hover:border-l-4 hover:border-[#8051FF] pl-2 flex gap-2'> Page-2</p>
+                                <p className='text-md font-roboto cursor-pointer hover:text-[#8051FF] hover:border-l-4 hover:border-[#8051FF] pl-2 flex gap-2'> page-3</p>
+                                <p className='text-md font-roboto cursor-pointer hover:text-[#8051FF] hover:border-l-4 hover:border-[#8051FF] pl-2 flex gap-2'> Page-4</p>
                             </div>
                         </div>
+                        )}
                     </span>
                 </a>
                 <a className='group text-[#4A4A4A] transition-all cursor-pointer duration-300 ease-in-out' href='#'>

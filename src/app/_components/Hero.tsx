c45ca8cca18a3114a138788f8a0f9afa5c45e5c0
utkarshkuhsi <utkarshsuneela@gmail.com>
2024-01-22 20:42:26 +0530

    adding background image for the hero section

---
+++ src/app/_components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Hero = (props: Props) => {
+  return (
+    <div className="h-[400px] bg-[url('/bg.png')] bg-[#FEA919]/5">
+        
+    </div>
+  )
+}
+
+export default Hero
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import Navbar from "./_components/Navbar";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
-    <Navbar /> 
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+    
+  
   );
 }
